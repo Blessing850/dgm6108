022400162670fabc912d1a0b2207df92507387c2
Blessing850 <amadi.b@northeastern.edu>
2026-01-15 13:05:36 -0800

    Update main.js

--- draft/main.js
+++ draft/main.js
@@ -20,92 +20,92 @@ let border = drawing.append("rect")
 /* Write your code for Project 1 beneath this comment */
 
 // Side body bumps (the dark green semi-circles)
-drawing.append("circle")
+let leftsideBody = drawing.append("circle")
     .attr("cx", 180)
     .attr("cy", 360)
     .attr("r", 40)
-    .attr("fill", "lemon");
-drawing.append("circle")
+    .attr("fill", "green");
+let rightsideBody = drawing.append("circle")
     .attr("cx", 320)
     .attr("cy", 360)
     .attr("r", 40)
-    .attr("fill", "lemon");
+    .attr("fill", "green");
 
-// 4. Main Body (The light green rectangle)
-drawing.append("rect")
+// Main Body (The light green rectangle)
+let mainBody = drawing.append("rect")
     .attr("x", 175)
     .attr("y", 250)
     .attr("width", 150)
     .attr("height", 150)
-    .attr("fill", "#8BC34A");
+    .attr("fill", "darkgreen");
 
-// 5. Eyes (The orange circles with black pupils)
+//  Eyes (The orange circles with black pupils)
 // Left Eye
-drawing.append("circle")
+let leftEye1 = drawing.append("circle")
     .attr("cx", 200)
     .attr("cy", 250)
     .attr("r", 30)
     .attr("fill", "pink");
-drawing.append("circle")
+let leftEye2 = drawing.append("circle")
     .attr("cx", 200)
     .attr("cy", 250)
     .attr("r", 10)
     .attr("fill", "black");
 // Right Eye
-drawing.append("circle")
+let rightEye1 = drawing.append("circle")
     .attr("cx", 300)
     .attr("cy", 250)
     .attr("r", 30)
     .attr("fill", "pink");
-drawing.append("circle")
+let rightEye2 = drawing.append("circle")
     .attr("cx", 300)
     .attr("cy", 250)
     .attr("r", 10)
     .attr("fill", "black");
 
-// 6. Nostrils (Tiny black dots)
-drawing.append("circle")
-    .attr("cx", 235)
+// Nostrils (Tiny black dots)
+let leftNostril = drawing.append("circle")
+    .attr("cx", 245)
     .attr("cy", 280)
     .attr("r", 4)
     .attr("fill", "black");
-drawing.append("circle")
-    .attr("cx", 265)
+let rightNostril = drawing.append("circle")
+    .attr("cx", 275)
     .attr("cy", 280)
     .attr("r", 4)
     .attr("fill", "black");
 
-// 7. Inner Legs (The two blueish/green vertical rectangles)
-drawing.append("rect")
+// Inner Legs (The two blueish/green vertical rectangles)
+let leftinnerLeg = drawing.append("rect")
     .attr("x", 210)
     .attr("y", 340)
     .attr("width", 15)
     .attr("height", 60)
     .attr("fill", "skyblue");
-drawing.append("rect")
+let righinnerLeg = drawing.append("rect")
     .attr("x", 275)
     .attr("y", 340)
     .attr("width", 15)
     .attr("height", 60)
     .attr("fill", "skyblue");
 
-// 8. Feet (Small orange circles at the bottom)
-drawing.append("circle")
+// Feet (Small orange circles at the bottom)
+let feet1 = drawing.append("circle")
     .attr("cx", 195)
     .attr("cy", 405)
     .attr("r", 8)
     .attr("fill", "yellow");
-drawing.append("circle")
+let feet2 = drawing.append("circle")
     .attr("cx", 220)
     .attr("cy", 405)
     .attr("r", 8)
     .attr("fill", "yellow");
-drawing.append("circle")
+let feet3 = drawing.append("circle")
     .attr("cx", 280)
     .attr("cy", 405)
     .attr("r", 8)
     .attr("fill", "yellow");
-drawing.append("circle")
+let feet4 = drawing.append("circle")
     .attr("cx", 310)
     .attr("cy", 405)
     .attr("r", 8)
